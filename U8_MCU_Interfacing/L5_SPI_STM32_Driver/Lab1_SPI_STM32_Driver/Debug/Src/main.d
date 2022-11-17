a4117c364b94318b49ee8f30d1bf84bb97acb871
Ahmed Aboraya <54743198+AhmedAboraya99@users.noreply.github.com>
2022-11-17 08:22:08 +0200

    STM32 SPI Labs
    -Lab1: Write on SPI Lines using USART through virtual Terminal
    -Lab2: Write on SPI Master using USART through virtual Terminal then data displayed on another VT of the Slave
    Simulation Videos:
    https://drive.google.com/drive/folders/1pt2PlAUAyuSm6hbKU5n0XHUPWmDxlUmV?usp=sharing

---
+++ U8_MCU_Interfacing/L5_SPI_STM32_Driver/Lab1_SPI_STM32_Driver/Debug/Src/main.d
@@ -0,0 +1,27 @@
+Src/main.o: ../Src/main.c ../Inc/STM32f103X6.h \
+ ../Inc/STM32f103X6_GPIO_Driver.h ../Inc/STM32F103x6.h \
+ E:/Work/Embedded\ systems/Diploma/embedded_system_online_diploma/U8_MCU_Interfacing/L5_SPI_STM32_Driver/Lab1_SPI_STM32_Driver/STM32_F103C6_Drivers/SPI\STM32F103CX_SPI_Driver.c \
+ ../Inc/STM32F103x8_UART_Driver.h ../Inc/STM32F103x6_GPIO_Driver.h \
+ ../Inc/STM32f103x6_GPIO_Driver.h ../Inc/STM32f103x8_RCC_Driver.h \
+ ../Inc/STM32F103Cx_SPI_Driver.h \
+ E:/Work/Embedded\ systems/Diploma/embedded_system_online_diploma/U8_MCU_Interfacing/L5_SPI_STM32_Driver/Lab1_SPI_STM32_Driver/STM32_F103C6_Drivers/UART\STM32F103x8_UART_Driver.c \
+ ../Inc/STM32F103X8_UART_Driver.h \
+ E:/Work/Embedded\ systems/Diploma/embedded_system_online_diploma/U8_MCU_Interfacing/L5_SPI_STM32_Driver/Lab1_SPI_STM32_Driver/STM32_F103C6_Drivers/GPIO\STM32f103X6_GPIO_Driver.c \
+ ../Inc/STM32F103X6_GPIO_Driver.h \
+ E:/Work/Embedded\ systems/Diploma/embedded_system_online_diploma/U8_MCU_Interfacing/L5_SPI_STM32_Driver/Lab1_SPI_STM32_Driver/STM32_F103C6_Drivers/RCC\STM32F103X8_RCC_Driver.c \
+ ../Inc/STM32F103X8_RCC_Driver.h
+../Inc/STM32f103X6.h:
+../Inc/STM32f103X6_GPIO_Driver.h:
+../Inc/STM32F103x6.h:
+E:/Work/Embedded\ systems/Diploma/embedded_system_online_diploma/U8_MCU_Interfacing/L5_SPI_STM32_Driver/Lab1_SPI_STM32_Driver/STM32_F103C6_Drivers/SPI\STM32F103CX_SPI_Driver.c:
+../Inc/STM32F103x8_UART_Driver.h:
+../Inc/STM32F103x6_GPIO_Driver.h:
+../Inc/STM32f103x6_GPIO_Driver.h:
+../Inc/STM32f103x8_RCC_Driver.h:
+../Inc/STM32F103Cx_SPI_Driver.h:
+E:/Work/Embedded\ systems/Diploma/embedded_system_online_diploma/U8_MCU_Interfacing/L5_SPI_STM32_Driver/Lab1_SPI_STM32_Driver/STM32_F103C6_Drivers/UART\STM32F103x8_UART_Driver.c:
+../Inc/STM32F103X8_UART_Driver.h:
+E:/Work/Embedded\ systems/Diploma/embedded_system_online_diploma/U8_MCU_Interfacing/L5_SPI_STM32_Driver/Lab1_SPI_STM32_Driver/STM32_F103C6_Drivers/GPIO\STM32f103X6_GPIO_Driver.c:
+../Inc/STM32F103X6_GPIO_Driver.h:
+E:/Work/Embedded\ systems/Diploma/embedded_system_online_diploma/U8_MCU_Interfacing/L5_SPI_STM32_Driver/Lab1_SPI_STM32_Driver/STM32_F103C6_Drivers/RCC\STM32F103X8_RCC_Driver.c:
+../Inc/STM32F103X8_RCC_Driver.h:
